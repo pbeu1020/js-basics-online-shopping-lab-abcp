9dc30a3e2ed738b05c60e6a3d8774ad79e995555
Peter Babeu <mbeenpete@hotmail.com>
2018-02-04 19:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,15 +65,16 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if (cart.hasOwnProperty(item)===false) {
-    console.log ("That item is not in your cart");
-  }
+  
   var i = 0;
   for(i=0; i < cart.length;  i++){
     if (cart[i].hasOwnProperty(item)){
       cart.splice (i, 1);
       
     }
+  }
+  if (cart.hasOwnProperty(item)===false) {
+    console.log ("That item is not in your cart");
   }
   return cart  ;
 }
