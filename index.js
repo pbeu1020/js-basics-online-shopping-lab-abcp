4622bec93cf4ca101fa3bfb851b87e7cb5e20d30
Peter Babeu <mbeenpete@hotmail.com>
2018-02-04 20:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   //var totalCost = total();
-  if (cardNumber === "") {
+  if (cardNumber === null) {
     return("Sorry, we don't have a credit card on file for you.");
   }  
     else
